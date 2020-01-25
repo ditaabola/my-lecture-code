30b580aa1322ea24bea48ceefeb20350f28041e1
ditaabola <dita.abola@gmail.com>
2020-01-25 14:53:07 +0200

    lecture7

---
+++ lecture7/src/lv/dita/repeat/TestJava.java
@@ -0,0 +1,23 @@
+package lv.dita.repeat;
+
+import java.util.Scanner;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Input value: ");
+		int inputvalue;
+		inputvalue = input.nextInt();
+		System.out.println("Your input value is " + inputvalue);
+	
+		if(!(input.hasNextInt())){
+			System.out.println("Input integer ");
+				
+			}
+		}
+	}			
+
+		
+	
+
