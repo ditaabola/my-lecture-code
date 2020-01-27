06fbf28da1e22d22ef8925dd9b3ea61d90eb78f7
ditaabola <60191682+ditaabola@users.noreply.github.com>
2020-01-27 11:34:38 +0200

    Update ScannerAndArrays.java

--- lecture7/src/lv/dita/repeatarray/ScannerAndArrays.java
+++ lecture7/src/lv/dita/repeatarray/ScannerAndArrays.java
@@ -14,10 +14,17 @@ public class ScannerAndArrays {
 	System.out.println("Enter one more integer: ");
 	arr[2] = sc.nextInt();
 	System.out.println("You entered an array of integers: " +arr[0] + " " + arr[1]+" " + arr[2]);
-	
-	
-	
-	
+		
+	Scanner sc2 = new Scanner(System.in);
+	System.out.println("Enter number of array elements you want to input: ");
+	int n = sc2.nextInt();
+	int a[] = new int[n];
+	System.out.println("Enter the elements of the array: ");
+	for(int i = 0; i<n; i++) {
+		a[i] = sc2.nextInt();
+		}
+	System.out.println("You listed these numbers: " + Arrays.toString(a));
+		
 	}
 
 }
